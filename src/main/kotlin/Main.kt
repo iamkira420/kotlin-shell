05d135ea1c6db3edf15b686a4db3bc98b9b8b000
Lutho420 <lmboniswa01@gmail.com>
2025-02-07 15:08:59 +0200

    basic shell

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,6 @@ fun main() {
     // Uncomment this block to pass the first stage
     print("$ ")
 
-    readln() // Wait for user input
+    val command = readln() // Wait for user command
+    println("invalid command: $command not found")
 }
