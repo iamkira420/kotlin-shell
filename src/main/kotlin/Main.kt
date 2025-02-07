9888bd515954da6799639c7228d1d8741ff51ff6
Lutho420 <lmboniswa01@gmail.com>
2025-02-07 20:36:43 +0200

    type+all prev cmd's implemented using functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
+val builtInCommands = listOf("echo", "exit", "type")
+
 fun main() {
     // list of currently defined commands
-    val builtInCommands = listOf("echo", "exit 0", "type")
 
     while (true) {
         print("$ ")
         val input = readln() // Wait for user input command
+
         if (input == "exit 0") {    // exit if user input command is <--
                break
         } else {
@@ -12,20 +14,29 @@ fun main() {
             val command = text[0]
             val commandArg = if (text.size > 1) text[1] else ""
 
-            if (command == "echo") { // implement echo functionality
-                println(commandArg)
-            } else {
-                println("$input: command not found")
-            }
+            // debugging: println("The $command command is a shell builtin and is defined: ${builtInCommands.contains(commandArg)}")
 
-            if (command == "type") {
-                if (builtInCommands.contains(commandArg)) {
+            if (command == "echo") { // implement echo functionality
+                println(echo(commandArg))
+            } else if (command == "type") {
+                /*if (builtInCommands.contains(commandArg)) {
                     println("$commandArg is a shell builtin")
                 } else {
                     println("$commandArg: not found")
-                }
+                }*/
+                println(type(commandArg))
+            } else {
+                println("$command: not found")
             }
         }
     }
+}
+
+fun type(str: String): String {
+    return if ( builtInCommands.contains(str)) "$str is a shell builtin" else "$str: not found"
+}
+
+fun echo(str: String): String {
+    return str
 }
 
